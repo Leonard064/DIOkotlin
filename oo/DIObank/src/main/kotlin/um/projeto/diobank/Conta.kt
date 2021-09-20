8d4e3c8dfbc58a1434d9eba1cdb8f5aff8e52263
Leonard064 <leonardo.nascibarbosa@outlook.com>
2021-09-20 13:33:53 -0300

    Criada classe Conta

---
+++ oo/DIObank/src/main/kotlin/um/projeto/diobank/Conta.kt
@@ -0,0 +1,13 @@
+package um.projeto.diobank
+
+import java.math.BigDecimal
+
+class Conta(val agencia: String, val numero: String, val saldo: BigDecimal ) {
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
--- oo/DIObank/src/main/kotlin/um/projeto/diobank/Pessoa.kt
+++ oo/DIObank/src/main/kotlin/um/projeto/diobank/Pessoa.kt
@@ -5,12 +5,13 @@ class Pessoa {
     var cpf: String = "123.456.789-10"
         private set
 
+    fun pessoaInfo() = "Nome: $nome \n CPF: $cpf"
+
 }
 
 fun main(){
     val teste = Pessoa()
 
-    println(teste.nome)
-    println(teste.cpf)
+    println(teste.pessoaInfo())
 
 }
