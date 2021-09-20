0f1eb0b83dc0444411398487e72a6237cc9ade24
Leonard064 <leonardo.nascibarbosa@outlook.com>
2021-09-20 11:37:10 -0300

    Alterações na classe Pessoa

--- oo/DIObank/src/main/kotlin/um/projeto/diobank/Pessoa.kt
+++ oo/DIObank/src/main/kotlin/um/projeto/diobank/Pessoa.kt
@@ -3,6 +3,10 @@ package um.projeto.diobank
 class Pessoa {
     var nome: String = "teste"
     var cpf: String = "123.456.789-10"
+
+    inner class Endereco{
+        var rua: String = "Rua ABC"
+    }
 }
 
 fun main(){
@@ -10,4 +14,6 @@ fun main(){
 
     println(teste.nome)
     println(teste.cpf)
+
+    println(teste.Endereco().rua)
 }
