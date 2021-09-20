15fd94f41ef98b609ef0d18f7a6c086a916c7234
Leonard064 <leonardo.nascibarbosa@outlook.com>
2021-09-20 11:20:57 -0300

    criado projeto inicial

---
+++ oo/DIObank/src/main/kotlin/um/projeto/diobank/Pessoa.kt
@@ -0,0 +1,13 @@
+package um.projeto.diobank
+
+class Pessoa {
+    var nome: String = "teste"
+    var cpf: String = "123.456.789-10"
+}
+
+fun main(){
+    val teste = Pessoa()
+
+    println(teste.nome)
+    println(teste.cpf)
+}
